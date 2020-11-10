2fd1204fde9096c463fef3328230b3cbd9e513cb
Luis Villén <l.villenjaldon@gmail.com>
2020-11-11 00:25:04 +0100

    Version 0.1. Function improved

--- deck.py
+++ deck.py
@@ -1,12 +1,9 @@
 suits = ['o', 'c', 'e', 'b']
 numbers = ['A', '2', '3', '4', '5', '6', '7', 'S', 'C', 'R']
 
-deck = []
-
 def createDeck():
+    deck = []       
     for suit in suits:
         for number in numbers:
             deck.append(number+suit)
     return deck 
-
-createDeck()
